691542e692ed0b7a80ca2de840dd115ecdb39836
Nychyporchuk <bloodever@gmail.com>
2024-10-14 14:39:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const bracketsMap = new Map(bracketsConfig);
+    const openBrackets = new Set(bracketsMap.keys());
+    const closeBrackets = new Set(bracketsMap.values());
+
+    const sameBrackets = {};
+    bracketsConfig.forEach(([open, close]) => {
+        if (open === close) sameBrackets[open] = 0;
+    });
+
+    for (let char of str) {
+
+        if (openBrackets.has(char)) {
+
+            if (char in sameBrackets) {
+
+                if (sameBrackets[char] % 2 === 0) {
+                    stack.push(char);
+                } else {
+
+                    if (stack.pop() !== char) return false;
+                }
+                sameBrackets[char]++;
+            } else {
+
+                stack.push(char);
+            }
+        } else if (closeBrackets.has(char)) {
+
+            const lastOpen = stack.pop();
+            if (bracketsMap.get(lastOpen) !== char) return false;
+        }
+    }
+
+
+    return stack.length === 0;
 }
